ff281e7535b2a07c37997239d74c521937e0a3f3
alvleont <alvleont@users.noreply.github.com>
2023-07-11 21:48:54 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,7 +23,7 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        $migration = include __DIR__ . '/../database/migrations/create_printables_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_printables_table.php.stub';
         $migration->up();
     }
 }
