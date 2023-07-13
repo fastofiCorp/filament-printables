c7d492717400fef9dce29a26ab01926bc69c9776
Alvaro León Torres <alletor@gmail.com>
2023-07-13 13:05:32 -0500

    Update PrintInput.php

--- src/Actions/PrintInput.php
+++ src/Actions/PrintInput.php
@@ -2,18 +2,25 @@
 
 namespace FastofiCorp\FilamentPrintables\Actions;
 
-use Barryvdh\DomPDF\Facade\Pdf;
 use Closure;
-use FastofiCorp\FilamentPrintables\Models\FilamentPrintable;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Filament\Forms\Components\Select;
-use Filament\Notifications\Notification;
-use Filament\Pages\Actions\Action;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
 use Ticketpark\HtmlPhpExcel\HtmlPhpExcel;
+use Filament\Forms\Components\Actions\Action;
+use FastofiCorp\FilamentPrintables\Models\FilamentPrintable;
 
 class PrintInput extends Action
 {
+
+    protected string|Closure $format = '';
+
+    protected int|Closure $printable = 0;
+
+    protected string|array|Closure $recordData = [];
+
     protected string|Closure|null $icon = 'heroicon-o-printer';
 
     public static function make(?string $name = 'print'): static
@@ -26,38 +33,75 @@ class PrintInput extends Action
         $this->label = __('filament-printables::filament-printables.resource.actions.print');
     }
 
+    public function format(string|Closure $format): static
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    public function printable(int|Closure $printable): static
+    {
+        $this->printable = $printable;
+
+        return $this;
+    }
+
+    public function data(string|array|Closure $recordData): static
+    {
+        $this->recordData = $recordData;
+        return $this;
+    }
+
     protected function setUp(): void
     {
         $this->modalWidth = 'sm';
         $this->action($this->handle(...));
     }
 
-    protected function handle(Model $record, array $data)
+    protected function handle(array $data)
     {
-        if (! isset($data['printable'])) {
+        if ($this->recordData != []) {
+            if (is_callable($this->recordData)) {
+                $record = call_user_func($this->recordData);
+            } else {
+                $record = $this->recordData;
+            }
+        }
+
+        if (isset($data['printable'])) {
+            $this->printable = $data['printable'];
+        }
+        if (isset($data['format'])) {
+            $this->format = $data['format'];
+        }
+
+        if ($this->printable == 0) {
             Notification::make('')->danger()
                 ->title(__('filament-printables::filament-printables.resource.notifications.no-template.title'))
                 ->body(__('filament-printables::filament-printables.resource.notifications.no-template.description'))
                 ->send();
         } else {
-            $printable = FilamentPrintable::find($data['printable']);
+
+            $printable = FilamentPrintable::find($this->printable);
             if ($printable) {
-                switch ($data['format']) {
+
+                switch ($this->format) {
                     case 'pdf':
 
                         return response()->streamDownload(function () use ($printable, $record) {
                             echo Pdf::loadHtml(
-                                Blade::render($printable->template_view, ['record' => $record], deleteCachedView: true)
+                                Blade::render($printable->template_view, $this->recordData, deleteCachedView: true)
                             )->stream();
-                        }, $printable->slug.'-'.$record->id.'.pdf');
+                        }, $printable->slug . '.pdf');
 
                     case 'xlsx':
 
                         return response()->streamDownload(function () use ($printable, $record) {
 
-                            $htmlPhpExcel = new HtmlPhpExcel(Blade::render($printable->template_view, ['record' => $record], deleteCachedView: true));
+                            $htmlPhpExcel = new HtmlPhpExcel(Blade::render($printable->template_view, $this->recordData, deleteCachedView: true));
                             echo $htmlPhpExcel->process()->output();
-                        }, $printable->slug.'-'.$record->id.'.xlsx');
+                        }, $printable->slug .  '.xlsx');
                 }
             }
         }
@@ -65,13 +109,20 @@ class PrintInput extends Action
 
     public function getFormSchema(): array
     {
+        //Get the printables linked to the resource
+        $printables = FilamentPrintable::where('type', 'form')->whereType('tag')->get();
 
-        $printables = FilamentPrintable::where('type', 'form')->whereJsonContains('linked_resources', $this->getModel())->get();
+        if ($this->printable != 0 and $this->format != '') {
+            return [];
+        }
+
         if ($printables->count() > 0) {
             return [
                 Select::make('printable')
                     ->label(__('filament-printables::filament-printables.resource.fields.template.label'))
                     ->options($printables->pluck('name', 'id')->toArray())
+                    ->default($this->printable != 0 ? $this->printable : null)
+                    ->disabled($this->printable != 0)
                     ->required()
                     ->reactive()
                     ->autofocus()
@@ -80,11 +131,13 @@ class PrintInput extends Action
                 Select::make('format')
                     ->label(__('filament-printables::filament-printables.resource.fields.format.label'))
                     ->required()
+                    ->default($this->format != '' ? $this->format : null)
+                    ->disabled($this->format != '')
                     ->options(function ($get) {
                         $options = [];
                         if ($get('printable') != '') {
                             collect(FilamentPrintable::find($get('printable'))?->format)->map(function ($format) use (&$options) {
-                                return $options[$format] = __('filament-printables::filament-printables.resource.fields.format.options.'.$format);
+                                return $options[$format] = __('filament-printables::filament-printables.resource.fields.format.options.' . $format);
                             });
                         }
 
